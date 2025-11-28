6ca1415bdbdb0bdd85c8851b806efc386454d431
Jon C <191928011+onthewallmj@users.noreply.github.com>
2025-11-28 15:10:45 -0500

    Refactor main.py to allow finer control of number of mahjong game simulations to run.

--- main.py
+++ main.py
@@ -2,21 +2,25 @@ from mahjong_game import MahjongGame
 from player import Player
 
 
-def main():
-    mahjong_game = MahjongGame(min_points_to_win=3, max_point_limit=13)
+def main(num_simulations: int = 1):
+    for i in range(num_simulations):
+        print(f"Starting Simulation {i + 1}...")
+        mahjong_game = MahjongGame(min_points_to_win=3, max_point_limit=13)
 
-    while True:
-        mahjong_game.play()
+        while True:
+            mahjong_game.play()
 
-        next_wind = mahjong_game.determine_table_wind()
+            next_wind = mahjong_game.determine_table_wind()
 
-        if next_wind == mahjong_game.table_wind.EAST and mahjong_game.get_game_count() > 0:
-            break
+            if next_wind == mahjong_game.table_wind.EAST and mahjong_game.get_game_count() > 0:
+                break
 
-    # Print out all player scores.
-    for idx, player in enumerate[Player](mahjong_game.players, 1):
-        print(f"Player {idx} score: {player.score}")
+        # Print out all player scores.
+        print(f"Simulation {i + 1} Results:")
+        for idx, player in enumerate(mahjong_game.players, 1):
+            print(f"Player {idx} score: {player.score}")
+        print()
 
 
 if __name__ == "__main__":
-    main()
+    main(1)
