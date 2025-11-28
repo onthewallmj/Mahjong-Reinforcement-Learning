d7418e7ecb59e957b0c8a7c4236c01ca8a9d7f25
Jon C <191928011+onthewallmj@users.noreply.github.com>
2025-11-28 15:51:51 -0500

    Export all classes and enums in mahjong/__init__.py

--- mahjong/__init__.py
+++ mahjong/__init__.py
@@ -1,5 +1,38 @@
-from .game import Game
-from .player import Player
-from .tile import Tile, TileSuit
 from .common import Wind
+from .game import Game
+from .game_history import GameHistory, GameResult
+from .hand_scorer import HandScorer
+from .meld import Meld, MeldType
+from .player import Player, PlayerGameState
+from .point_source import PointSource, PointType
+from .tile import (
+    DragonValue,
+    FlowerValue,
+    SeasonValue,
+    Tile,
+    TileSuit,
+    WindValue,
+)
+from .win import Win, WinCondition
 
+__all__ = [
+    "Wind",
+    "Game",
+    "GameHistory",
+    "GameResult",
+    "HandScorer",
+    "Meld",
+    "MeldType",
+    "Player",
+    "PlayerGameState",
+    "PointSource",
+    "PointType",
+    "DragonValue",
+    "FlowerValue",
+    "SeasonValue",
+    "Tile",
+    "TileSuit",
+    "WindValue",
+    "Win",
+    "WinCondition",
+]
